6423158f81bc74640a57978b988a184ab1a9c700
Rozhnov Alexandr <nox73@ya.ru>
2012-08-20 23:03:21 +0400

    vagrant sfk

---
+++ my-recipes/cookbooks/sfk/metadata.rb
@@ -0,0 +1,8 @@
+maintainer       "Alexandr Rozhnov"
+maintainer_email "gnox73@gmail.com"
+license          "MIT License"
+description      "my first reciept"
+version          "0.1"
+
+depends "apt-repo"
+depends "zeromq-cookbook"
---
+++ my-recipes/cookbooks/sfk/recipes/default.rb
@@ -0,0 +1,25 @@
+link "/etc/vpnc" do
+  to "/home/vagrant/Dropbox/etc/vpnc"
+end
+
+package "vpnc"
+package "vim"
+package "git-core"
+package "curl"
+package "libcairo2-dev"
+package "libpango1.0-dev"
+
+execute 'run vpnc' do
+  command 'vpnc'
+  action :run
+end
+
+include_recipe "apt"
+include_recipe "rvm::user"
+include_recipe "zeromq-cookbook::default"
+
+#group "rvm" do
+#  action :modify
+#  members "vagrant"
+#  append true
+#end
